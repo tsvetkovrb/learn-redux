a3bd1fa55be78766cafe1761919a0b0812168a59
Roman Tsvetkov <tsvetkovv22@gmail.com>
2018-12-05 10:20:32 +1000

    Подключил Redux DevTools

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 import App from "./App";
 
@@ -27,7 +27,9 @@ const logger = store => {
   };
 };
 
-const store = createStore(reducer, applyMiddleware(logger));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancers(applyMiddleware(logger)));
 
 ReactDOM.render(
   <Provider store={store}>
